07cba40c5449a172ac603ba939c875e23f9f7317
Andrey <ninzyago@gmail.com>
2026-02-11 11:32:32 +0300

    Leetcode daily

--- 3719.LongestBalancedSubarrayI.py
+++ 3719.LongestBalancedSubarrayI.py
@@ -23,6 +23,29 @@ class Solution:
                     ans = max(ans, r - l + 1)
 
         return ans
+    
+class FastestSolution:
+    def longestBalanced(self, nums: List[int]) -> int:
+        n = len(nums)
+        res = 0
+        seen = set()
+        for i in range(n):
+            seen.clear()
+            balance = 0
+            if res > n - i:
+                break
+            for j in range(i, n):
+                num = nums[j]
+                if num not in seen: 
+                    if num % 2:
+                        balance -= 1
+                    else:
+                        balance += 1
+                    seen.add(num)
+                if balance == 0:
+                    res = max(res, j - i + 1)
+        
+        return res
 
 
 if __name__ == "__main__":
