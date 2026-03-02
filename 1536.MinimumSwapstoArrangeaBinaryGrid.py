667c225a834760402372d2df9ec101ea95656d18
wave-or-corpuscula <ninzyago@gmail.com>
2026-03-02 19:10:48 +0300

    Leetcode daily

---
+++ 1536.MinimumSwapstoArrangeaBinaryGrid.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/minimum-swaps-to-arrange-a-binary-grid/description/?envType=daily-question&envId=2026-03-02
+
+from typing import List
+
+
+class Solution:
+    def minSwaps(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        zeros = []
+
+        for row in grid:
+            count = 0
+            for j in range(n - 1, -1, -1):
+                if row[j] == 0:
+                    count += 1
+                else:
+                    break
+            zeros.append(count)
+
+        swaps = 0
+
+        for i in range(n):
+            needed = n - i - 1
+            j = i
+            while j < n and zeros[j] < needed:
+                j += 1
+            if j == n:
+                return -1
+            while j > i:
+                zeros[j], zeros[j - 1] = zeros[j - 1], zeros[j]
+                j -= 1
+                swaps += 1
+
+        return swaps
