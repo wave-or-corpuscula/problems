d113f7c35d9080ab6fe25e4a97aa405ae2e691ca
wave-or-corpuscula <ninzyago@gmail.com>
2026-03-07 16:24:09 +0300

    Leetcode daily

---
+++ 1888.MinimumNumberofFlipstoMaketheBinaryStringAlternating.py
@@ -0,0 +1,19 @@
+# https://leetcode.com/problems/minimum-number-of-flips-to-make-the-binary-string-alternating/description/?envType=daily-question&envId=2026-03-07
+
+
+class Solution:
+    def minFlips(self, s: str) -> int:
+        n = len(s)
+        res = n
+        op = [0, 0]
+
+        for i in range(n):
+            op[(ord(s[i]) ^ i) & 1] += 1
+
+        for i in range(n):
+            c = ord(s[i])
+            op[(c ^ i) & 1] -= 1
+            op[(c ^ (n + i)) & 1] += 1
+            res = min(res, min(op))
+
+        return res
