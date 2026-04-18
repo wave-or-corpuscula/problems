0531ff0a1add9d7bdfa7fe60845397e01f444d9d
wave-or-corpuscula <ninzyago@gmail.com>
2026-04-18 21:42:01 +0300

    daily

--- 2463.MinimumTotalDistanceTraveled.py
+++ 2463.MinimumTotalDistanceTraveled.py
@@ -1,42 +1,44 @@
 # https://leetcode.com/problems/minimum-total-distance-traveled/description/?envType=daily-question&envId=2026-04-14
 
+from typing import List
+
 
 class Solution:
-    def minimumTotalDistance(self, robot, factory):
+    def minimumTotalDistance(
+        self, robots: List[int], factories: List[List[int]]
+    ) -> int:
         # Sort robots and factories by position
-        robot.sort()
-        factory.sort()
+        robots.sort()
+        factories.sort()
 
         # Flatten factory positions according to their capacities
         factory_positions = []
-        for f in factory:
-            for i in range(f[1]):
-                factory_positions.append(f[0])
+        for factory in factories:
+            for i in range(factory[1]):
+                factory_positions.append(factory[0])
 
-        # Recursively calculate minimum total distance
-        return self._calculate_min_distance(0, 0, robot, factory_positions)
+        robot_count = len(robots)
+        factory_count = len(factory_positions)
+        next_dist = [0 for _ in range(factory_count + 1)]
+        current = [0 for _ in range(factory_count + 1)]
 
-    def _calculate_min_distance(
-        self, robot_idx, factory_idx, robot, factory_positions
-    ):
-        # All robots assigned
-        if robot_idx == len(robot):
-            return 0
-        # No factories left to assign
-        if factory_idx == len(factory_positions):
-            return 1e12
+        current[factory_count] = 1e12
 
-        # Option 1: Assign current robot to current factory
-        assign = abs(
-            robot[robot_idx] - factory_positions[factory_idx]
-        ) + self._calculate_min_distance(
-            robot_idx + 1, factory_idx + 1, robot, factory_positions
-        )
+        # Fill DP table using two rows for optimization
+        for i in range(robot_count - 1, -1, -1):
+            for j in range(factory_count - 1, -1, -1):
+                # Assign current robot to current factory
+                assign = (
+                    abs(robots[i] - factory_positions[j]) + next_dist[j + 1]
+                )
 
-        # Option 2: Skip current factory for the current robot
-        skip = self._calculate_min_distance(
-            robot_idx, factory_idx + 1, robot, factory_positions
-        )
+                # Skip current factory for this robot
+                skip = current[j + 1]
+                # Take the minimum option
+                current[j] = min(assign, skip)
 
-        # Take the option with the minimum distance
-        return min(assign, skip)
+            # Move to next robot
+            next_dist = current[:]
+
+        # Return minimum distance starting from the first robot
+        return current[0]
